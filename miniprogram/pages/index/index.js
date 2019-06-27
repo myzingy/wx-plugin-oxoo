e554099b4251ef2c3a0e6b2672552bdbeeeb3c69
vking <251941666@qq.com>
2019-06-27 15:28:46 +0800

    wx-plugin-oxoo

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,13 @@
 var plugin = requirePlugin("myPlugin")
 Page({
+  data:{
+    qnConf:{
+      accessKey:'uva-aaonM8dukeR6phc9yuPBrOXk9brrNW_PxLD7',
+      secretKey:'oZMMVN6phk49lG0J1xwmi1Ju0ETT15EnAEWcU9Cs',
+      bucket:'colorcun-attr',
+      fileType:'image',
+    }
+  },
   onLoad: function() {
     plugin.getOpenid().then(res=>{
       console.log(res);
--- plugin/components/qiniu/upload.js
+++ plugin/components/qiniu/upload.js
@@ -1,17 +1,29 @@
+import cloud from '../../api/cloud'
 Component({
+  properties:{
+    qnConf:{
+      type:Object,
+      value:null
+    }
+  },
   data: {
-    user:null,
+
   },
   attached: async function(){
-    // 可以在这里发起网络请求获取插件的数据
-    //wx.cloud.init();
-    // let res=await wx.cloud.callFunction({
-    //   name: 'login',
-    //   data: {},
-    // })
-    // console.log('wx.cloud.login',res.result)
-    // this.setData({
-    //   user: res.result
-    // })
+    console.log('this.data.qnConf',this.data.qnConf)
+    cloud.getTokenQiniu(this.data.qnConf)
   },
+  methods:{
+    changeFile:(e)=>{
+      wx.chooseImage({
+        count:9,
+        sizeType:['compressed'],
+        sourceType:['album'],
+        success:res=>{
+          console.log('wx.chooseImage',res);
+        }
+      })
+    },
+  },
+
 })
