f62e8c30249b082b9769c6fab24a709ec482a9c7
vking <251941666@qq.com>
2019-07-02 18:46:02 +0800

    wx-plugin-oxoo

--- cloudfunctions/wxcloud/index.js
+++ cloudfunctions/wxcloud/index.js
@@ -1,19 +1,20 @@
 // 云函数入口函数
-var request = require('request');
+var got = require('got');
 const cloud = require('wx-server-sdk')
+cloud.init()
 exports.main = async (event, context) => {
-  console.log({event:event, context: context});
+  console.log({event:event});
   switch (event.act){
     case 'token':
-      return getToken(event);
+      return await getToken(event);
       break;
   }
 }
 //获取token
-function getToken(params){
+async function getToken(params){
   let response={code:300,msg:'参数为空'}
   if(params.qnConf){
-    cloud.callFunction({
+    let res=await cloud.callFunction({
       // 要调用的云函数名称
       name: 'qncloud',
       // 传递给云函数的event参数
@@ -21,20 +22,13 @@ function getToken(params){
         act: 'token',
         ...params.qnConf
       }
-    }).then(res => {
-      console.log('qncloud',res)
-    }).catch(err => {
-      console.log('qncloud',err)
     })
+    console.log('qncloud.res',res)
   }
   if(params.appid && params.secret){
-    request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+params.appid+'&secret='+params.secret
-      , function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        response=JSON.parse(body)
-        console.log(response)
-      }
-    })
+    let tokenUrl='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+params.appid+'&secret='+params.secret
+    let tokenResponse=await got(tokenUrl)
+    console.log('tokenResponse',tokenResponse.body)
   }
   return response;
 }
