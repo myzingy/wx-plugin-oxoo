23a8ce2eb19cfe4340733b3fae4fe62df3597dcc
vking <251941666@qq.com>
2019-07-03 15:09:25 +0800

    wx-plugin-oxoo

--- cloudfunctions/wxcloud/index.js
+++ cloudfunctions/wxcloud/index.js
@@ -1,6 +1,7 @@
 // 云函数入口函数
 var got = require('got');
 const cloud = require('wx-server-sdk')
+const FormData = require('form-data');
 cloud.init()
 exports.main = async (event, context) => {
   console.log({event:event});
@@ -8,11 +9,51 @@ exports.main = async (event, context) => {
     case 'token':
       return await getToken(event);
       break;
+    case 'qrcode':
+      return await getQrcode(event);
   }
 }
 //获取token
 async function getToken(params){
   let response={code:300,msg:'参数为空'}
+  if(params.appid && params.secret){
+    const db = cloud.database()
+    const _ = db.command
+    let res=await db.collection('wxtoken').where({
+      appid: params.appid
+    }).get()
+    let now=parseInt(new Date()/1000)
+    console.log('dbres',res)
+    if(res.data.length>0){
+      let wxtoken=res.data[0]
+      if(wxtoken.expires_in>now){
+        return {
+          code:200,
+          access_token:wxtoken.access_token
+        }
+      }
+      await db.collection('wxtoken').where({
+        appid: params.appid
+      }).remove()
+    }
+    let tokenUrl='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+params.appid+'&secret='+params.secret
+    let tokenResponse=await got(tokenUrl)
+    console.log('tokenResponse',tokenResponse.body)
+    response=JSON.parse(tokenResponse.body)
+    response.code=200
+    await db.collection('wxtoken').add({
+      data:{
+        appid: params.appid,
+        access_token:response.access_token,
+        expires_in:parseInt(response.expires_in)+now,
+      }
+    })
+  }
+  return response;
+}
+async function getQrcode(params){
+  let response={code:300,msg:'参数为空'}
+  let qnres
   if(params.qnConf){
     let res=await cloud.callFunction({
       // 要调用的云函数名称
@@ -24,11 +65,34 @@ async function getToken(params){
       }
     })
     console.log('qncloud.res',res)
+    qnres=res
   }
-  if(params.appid && params.secret){
-    let tokenUrl='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+params.appid+'&secret='+params.secret
-    let tokenResponse=await got(tokenUrl)
-    console.log('tokenResponse',tokenResponse.body)
+  let tokenres=await getToken(params);
+
+  const form = new FormData();
+
+  form.append('access_token', tokenres.access_token);
+  form.append('path', params.path);
+  if(params.width) form.append('width', params.width);
+  if(params.line_color) form.append('line_color', params.line_color);
+  if(params.is_hyaline) form.append('is_hyaline', params.is_hyaline);
+
+  console.log("\n\r",{
+    form:form,
+    access_token:tokenres.access_token,
+  })
+  let qrres=await got.post('https://api.weixin.qq.com/wxa/getwxacode?access_token='+tokenres.access_token,{
+    headers:{
+      //accept:'application/json',
+      //'Content-Type':'application/x-www-form-urlencoded',
+    },
+    body:form,
+  })
+  console.log("\n\r",{
+    qrres:qrres,
+  })
+  if(qnres){//上传到七牛
+
   }
-  return response;
+  return qrres
 }
